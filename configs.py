ffaed94feef28f997a3c8e28bd1e746b2e22af9d
Cinemahubbots <126318209+Cinemahubbots@users.noreply.github.com>
2023-03-04 15:18:09 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -1,12 +1,11 @@
 from os import path, getenv
 
 class Config:
-    API_ID = int(getenv("API_ID", "0112234"))
-    API_HASH = getenv("API_HASH", "abcdefg")
-    BOT_TOKEN = getenv("BOT_TOKEN", "1234567891:AdDfgFRFVVfDEhdhyjjvjjftSEW")
-    FSUB = getenv("FSUB", "SDBotz")
-    CHID = int(getenv("CHID", "-1000112234"))
-    SUDO = list(map(int, getenv("SUDO").split()))
-    MONGO_URI = getenv("MONGO_URI", "")
+    API_ID = int(getenv("API_ID", "22359038"))
+    API_HASH = getenv("API_HASH", "b3901895dc193c30c808ba4f1b550ed0")
+    BOT_TOKEN = getenv("BOT_TOKEN", "5630873709:AAHtEhH9rsZslFQw3dHngt37jjwkNdK16qc")
+    CHID = int(getenv("CHID", None))
+    SUDO = list(map(int, getenv("SUDO").split(5531461861)))
+    MONGO_URI = getenv("MONGO_URI", "mongodb+srv://Adish4:Adish4@cluster0.dyhrj0e.mongodb.net/?retryWrites=true&w=majority")
     
 cfg = Config()
